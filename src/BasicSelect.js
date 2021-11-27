6703a892474ab08265adb62006b7315e7eb5c3b4
Thomas Marwitz <thomasmarwitz3@gmail.com>
2021-11-27 09:47:16 +0100

    year input

--- src/BasicSelect.js
+++ src/BasicSelect.js
@@ -8,26 +8,46 @@ import modelList from '../src/ressources/models.json'
 
 export default function BasicSelect() {
   const [model, setModel] = React.useState('');
+  const [year, setYear] = React.useState('');
 
-  const handleChange = (event) => {
+  const handleModelChange = (event) => {
     setModel(event.target.value);
     console.log(model)
   };
 
+  const handleYearChange = (event) => {
+    setYear(event.target.value);
+    console.log(year)
+  }
+
   const items = modelList.map(model => <MenuItem value={model}>{model}</MenuItem>)  
+  const validYears = [...Array(141).keys()].map(value => `${value + 1960}`) // 1960 - 2100
+  const years = validYears.map(year => <MenuItem value={year}>{year}</MenuItem>)
 
   return (
     <Box sx={{ minWidth: 300 }}>
       <FormControl fullWidth>
-        <InputLabel id="demo-simple-select-label">Model</InputLabel>
+        <InputLabel id="model-select-label">Model</InputLabel>
         <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
+          labelId="model-select-label"
+          id="model-select"
           value={model}
           label="Model"
-          onChange={handleChange}
+          onChange={handleModelChange}
         >
           {items}
+        </Select>
+      </FormControl>
+      <FormControl fullWidth>
+      <InputLabel id="year-select-label">Year</InputLabel>
+        <Select
+          labelId="year-select-label"
+          id="year-select"
+          value={year}
+          label="Year"
+          onChange={handleYearChange}
+        >
+          {years}
         </Select>
       </FormControl>
     </Box>
