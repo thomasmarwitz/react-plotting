ea71d0a134ec248c80f17e7475f80acba5b9ead1
Thomas Marwitz <thomasmarwitz3@gmail.com>
2021-11-27 14:33:17 +0100

    working example

--- src/Graph.js
+++ src/Graph.js
@@ -1,48 +1,83 @@
-import React, { Component } from "react"
-import { Line } from "react-chartjs-2"
-
-const styles = {
-    display: "flex",
-    justifyContent: "center",
-}
-
-export default function Graph(props) {
-        
-    /*
-    const data2 = {
-    labels: [],// years,
-    datasets: [
-          {
-              label: "",// name
-              data: [], // y values
-              borderColor: "hex",
-              fill: false,
-          }
-        ]
-    };*/
-
-    const data = {
-        labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
-        datasets: [
-          {
-            label: "Line 1",
-            data: [33, 25, 35, 51, 54, 76],
-            fill: false,
-            borderColor: "#742774"
-          }
-        ]
-    };
-
-
-
+import {
+    LineChart,
+    Line,
+    XAxis,
+    YAxis,
+    CartesianGrid,
+    Tooltip,
+    Legend
+  } from "recharts";
+  
+  const data = [
+    {
+      name: "Page A",
+      uv: 4000,
+      pv: 2400,
+      amt: 2400
+    },
+    {
+      name: "Page B",
+      uv: 3000,
+      pv: 1398,
+      amt: 2210
+    },
+    {
+      name: "Page C",
+      uv: 2000,
+      pv: 9800,
+      amt: 2290
+    },
+    {
+      name: "Page D",
+      uv: 2780,
+      pv: 3908,
+      amt: 2000
+    },
+    {
+      name: "Page E",
+      uv: 1890,
+      pv: 4800,
+      amt: 2181
+    },
+    {
+      name: "Page F",
+      uv: 2390,
+      pv: 3800,
+      amt: 2500
+    },
+    {
+      name: "Page G",
+      uv: 3490,
+      pv: 4300,
+      amt: 2100
+    }
+  ];
+  
+  export default function Graph() {
     return (
-        <div style={styles}>
-            <Line
-                data={data}
-                width={400}
-                height={400}
-                options={{}}
-            />
-        </div>
-    )
-}
+      <LineChart
+        width={500}
+        height={300}
+        data={data}
+        margin={{
+          top: 5,
+          right: 30,
+          left: 20,
+          bottom: 5
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Line
+          type="monotone"
+          dataKey="pv"
+          stroke="#8884d8"
+          activeDot={{ r: 8 }}
+        />
+        <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+      </LineChart>
+    );
+  }
