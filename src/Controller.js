f7d805eac9dd2eca23b7c67d4782fe1796330031
Thomas Marwitz <thomasmarwitz3@gmail.com>
2021-11-27 09:20:04 +0100

    dropdown trying

--- src/Controller.js
+++ src/Controller.js
@@ -1,19 +1,41 @@
 import React, { Component } from "react"
 import Button from '@mui/material/Button';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
 
 const styles = {
-    display: "flex",
-    justifyContent: "center",
+//    display: "flex",
+//    justifyContent: "center",
 }
 
 class Controller extends Component {
     constructor(props) {
         super(props)
+        this.handleChange = this.handleChange.bind(this)
+        this.state = {
+            age: "10"
+        }
+    }
+
+    handleChange(event) {
+        this.state.age = event.target.value 
     }
 
     render() {
         return <div style={styles}>
-            <Button variant="containted">Calculate Offset</Button>
+            <Select 
+                labelId="something"
+                id="demo-simple-select"
+                value={this.age}
+                label="Label"
+                sx={{minWidth: 200}}
+                onChange={this.handleChange}
+            >
+                <MenuItem value={10}>Ten</MenuItem>
+                <MenuItem value={20}>Twenty</MenuItem>
+                <MenuItem value={30}>Thirty</MenuItem>
+            </Select>
+            <Button variant="contained">Calculate + Offset</Button>
         </div>
     }
 }
