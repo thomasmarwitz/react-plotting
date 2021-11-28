eecacf0ec97aee91949335ee9ec2e612858d5f96
Thomas Marwitz <thomasmarwitz3@gmail.com>
2021-11-28 11:08:56 +0100

    comments

--- src/Graph.js
+++ src/Graph.js
@@ -23,8 +23,8 @@ ChartJS.register(
 );
 
 const styles = {
-    "maxWidth": 1200,
-    "maxHeight": 500,
+    //"maxWidth": 1200,
+    //"maxHeight": 500,
 }
 
 const options = {
@@ -40,25 +40,6 @@ const options = {
   },
 };
 
-// labels = years
-const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-
-const data = {
-  labels,
-  datasets: [
-
-    {
-      label: 'Dataset 1',
-      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
-      borderColor: 'rgb(255, 99, 132)',
-    },
-    {
-      label: 'Dataset 2',
-      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
-      borderColor: 'rgb(53, 162, 235)',
-    },
-  ],
-};
 
 export default function Graph(props) {
   return <Line style={styles} options={options} data={props.data} />;
